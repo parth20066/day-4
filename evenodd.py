0e5228d8825a33d0cccf37bc9ad4dcd73e855f24
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-13 12:19:41 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,17 @@
+#even or odd
+a=int(input("enter the number"))
+def number(number):
+ if a%2==0:
+   print("even number")
+ else:
+   print("odd number")
+number(a)
+enter the number25
+odd number
+
+import cat
+cat.number(a)
+
+enter the number25
+odd number
+odd number
