d52d4396a8d40b8e3722f8b9957d0aa569abe1ff
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-13 12:18:47 +0530

    Create area of circle in coustum module.py

---
+++ area of circle in coustum module.py
@@ -0,0 +1,14 @@
+def area_of_circle(pi):
+ radius=int(input("enter the number"))
+ area_of_circle=3.14*radius*radius
+ print(area_of_circle)
+area_of_circle(3.14)
+enter the number25
+1962.5
+
+import area
+area.area_of_circle(3.14)
+enter the number25
+1962.5
+enter the number26
+2122.64
